506f13d09e6cfcca0e602bb04ddfc19a2e9f9f97
SashaT9 <oleksandr.tymkovych@student.uj.edu.pl>
2025-06-06 05:51:07 +0200

    final commit

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# chat-app
+# random-chat
+
+A simple CLI chat application. Written in Go for learning purposes.
+
+# Installation
+```bash
+git clone https://github.com/SashaT9/random-chat.git
+cd random-chat
+go install .
+```
+# Usage
+```bash
+random-chat --help
+```
--- cmd/root.go
+++ cmd/root.go
@@ -20,13 +20,8 @@ var (
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "random-chat",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Chat with random people",
+	Long:  ``,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	RunE: func(cmd *cobra.Command, args []string) error {
